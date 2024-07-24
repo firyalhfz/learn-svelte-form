6de92bce5dc2fb2033b36229568eea0607d24946
firyalhfz <iyaalfiryal@gmail.com>
2024-07-24 15:02:51 +0700

    snapshot

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+  let name = '';
+  let email = '';
+  let message = '';
+
+  export const snapshot = {
+    capture: () => {
+      return {
+        name,
+        email,
+        message
+      };
+    },
+    restore: (data) => {
+      name = data.name;
+      email = data.email;
+      message = data.message;
+    }
+  };
+</script>
+
+<h1>contact form</h1>
+
+<form method="post">
+  <div>
+    <input type="text" name="name" placeholder="Name" bind:value={name} />
+  </div>
+  <div>
+    <input type="text" name="email" placeholder="Email" bind:value={email} />
+  </div>
+  <div>
+    <textarea name="message" placeholder="Message" bind:value={message} />
+  </div>
+  <button type="submit">Submit</button>
+  <a href="/">cancel</a>
+</form>
