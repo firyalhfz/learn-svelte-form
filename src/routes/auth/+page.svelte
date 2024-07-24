fc5d5be3dd91d7c670a8e7af805b8930c9763ce9
firyalhfz <iyaalfiryal@gmail.com>
2024-07-24 13:00:05 +0700

    default form actions

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,33 +1,24 @@
 <script>
-  let username = '';
-  let password = '';
-  let message = '';
+  //   let username = '';
+  //   let password = '';
+  //   let message = '';
 
-  const handleSubmit = async () => {
-    message = '';
-    const response = await fetch('/auth', {
-      method: 'POST',
+  //   const handleSubmit = async () => {
+  //     message = '';
+  //     const response = await fetch('/auth', {
+  //       method: 'POST',
 
-      body: JSON.stringify({ username, password })
-    });
-    const data = await response.json();
-    message = data.message;
-  };
+  //       body: JSON.stringify({ username, password })
+  //     });
+  //     const data = await response.json();
+  //     message = data.message;
+  //   };
+  export let form;
 </script>
 
-<form on:submit|preventDefault={handleSubmit}>
-  <p>{message}</p>
-  <input
-    type="text"
-    name="username"
-    placeholder="Username"
-    bind:value={username}
-  />
-  <input
-    type="password"
-    name="password"
-    placeholder="Password"
-    bind:value={password}
-  />
+<form method="post">
+  <p>{form?.message || ''}</p>
+  <input type="text" name="username" placeholder="Username" />
+  <input type="password" name="password" placeholder="Password" />
   <button type="submit">Login</button>
 </form>
